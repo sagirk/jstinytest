7fa623d7fec5ec8bfe69fee8a5ad242335ff8625
Sagir Khan <sagir.khan@gmail.com>
2017-05-20 19:54:37 +0530

    Added test to check if reduce() returns reduced value. Test passing.

--- wc-tdd/reduce/reduce.js
+++ wc-tdd/reduce/reduce.js
@@ -5,13 +5,13 @@ function reduce (array, callback, initialValue) {
     accumulator = initialValue;
 
     for (var i = 0; i < array.length; i++) {
-      callback(accumulator, array[i], i, array);
+      accumulator = callback(accumulator, array[i], i, array);
     }
   } else {
     accumulator = array[0];
 
     for (var i = 1; i < array.length; i++) {
-      callback(accumulator, array[i], i, array);
+      accumulator = callback(accumulator, array[i], i, array);
     }
   }
 
