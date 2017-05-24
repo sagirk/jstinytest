9a2bf6613bcf21dbe42b018c292d07710c376024
Sagir Khan <sagir.khan@gmail.com>
2017-05-24 07:36:29 +0530

    Refactored reduce after finishing TDD 9. Archived old code.

--- wc-tdd/reduce/reduce.js
+++ wc-tdd/reduce/reduce.js
@@ -1,27 +1,44 @@
-function reduce (array, callback, initialValue) {
-  var accumulator;
+function reduce(array, callback, initialValue) {
+  var assignedIndexes = Object.keys(array);
+  var startingIndex = assignedIndexes[0];
+  var accumulator = initialValue;
+  // Persist length of input array at the beginning to safeguard 
+  // against it being modified later by the callback, etc.
+  var length = array.length;
 
-  if (typeof initialValue !== 'undefined') {
-    accumulator = initialValue;
+  // No initialValue
+  if (arguments.length < 3) {
+    if (!assignedIndexes.length) throw new TypeError('Reduce of empty array with no initial value');
 
-    for (var i = 0; i < array.length; i++) {
-      if (i in array) {
-        accumulator = callback(accumulator, array[i], i, array);
-      }
-    }
+    // TODO: Get a code review from Gordon to validate if skipping the while
+    // loop in favor of a non-loop soultion was a better approach.
+
+    // while (startingIndex in array === false && startingIndex < length) {
+    //   startingIndex++;
+    // }
+
+    // if (assignedIndexes.length === 1) {
+    //   var onlyIndex = assignedIndexes[0];
+    //   var onlyElement = array[onlyIndex];
+    //   return onlyElement;
+    // }
+
+    accumulator = array[startingIndex];
+
+    if (assignedIndexes.length === 1) return accumulator;
+
+    startingIndex++;
+
+  // Has initialValue
   } else {
-    firstAssignedIndex = array.findIndex(function (element) {
-      return element !== undefined;
-    });
+    if (!assignedIndexes.length) return accumulator;
+  }
 
-    accumulator = array[firstAssignedIndex];
-
-    for (var i = firstAssignedIndex+1; i < array.length; i++) {
-      if (i in array) {
-        accumulator = callback(accumulator, array[i], i, array);
-      }
+  for (var i = startingIndex; i < length; i++) {
+    if (i in array) {
+      accumulator = callback(accumulator, array[i], i, array);
     }
   }
 
   return accumulator;
-};
+}
